7998552df5028fa44e4110c6d866bec9d6e55cce
Awes Khan <44608315+Awes-Khan@users.noreply.github.com>
2024-08-16 21:49:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 function mincost(arr)
 { 
 //write your code here
+    arr.sort((a, b) => a - b); 
+    let total = 0;
+
+    while (arr.length > 1) {
+        let a = arr.shift();
+        let b = arr.shift();
+        let c = a + b;
+        total += c;
+        arr.push(cost);
+        arr.sort((a, b) => a - b);
+    }
+    return total;
 // return the min cost
   
 }
